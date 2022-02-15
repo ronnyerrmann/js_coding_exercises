c5b3de9b96e791e7f4c3e33d230f667cc7d0db3f
ronnyerrmann <36272471+ronnyerrmann@users.noreply.github.com>
2022-02-15 12:25:46 +0000

    Finished Exercise 2

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,35 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  // Check that the key is available
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  // Check that the key is available
+  return (person.city.toLowerCase() == "manchester");
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  return Math.ceil(people/40);
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  let numberOfSheep = 0;
+  for (let entry of arr) {
+    if (entry.toLowerCase() == "sheep") {
+      numberOfSheep ++;
+    }
+  }
+  return numberOfSheep;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  // Check that the key is available
+  return (person.address.city.toLowerCase() == "manchester") && (person.address.postCode.slice(0, 1).toLowerCase() == "m");
 }
 
 module.exports = {
