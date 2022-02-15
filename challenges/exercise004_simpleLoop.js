1bc6d1becbdd6cb7395c78908535b1c0474075a2
ronnyerrmann <36272471+ronnyerrmann@users.noreply.github.com>
2022-02-15 17:19:36 +0000

    forEach loops for exercise 4

--- challenges/exercise004_simpleLoop.js
+++ challenges/exercise004_simpleLoop.js
@@ -7,11 +7,11 @@ function roundTo(number, digits) {
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
   let smallNums = [];
-  nums.forEach(function(num) {
+  for (let num of nums) {
     if (num < 1) {
       smallNums.push(num);
     }
-  });
+  }
   return smallNums;
 }
 
@@ -19,51 +19,51 @@ function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
   let resultNames = [];
-  names.forEach(function(name) {
+  for (let name of names) {
     if (name.slice(0,1) == char) {
       resultNames.push(name);
     }
-  });
+  }
   return resultNames;
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
   let verbs = [];
-  words.forEach(function(word) {
+  for (let word of words) {
     if (word.slice(0,3) == "to ") {
       verbs.push(word);
     }
-  });
+  }
   return verbs;
 }
 
 function getIntegers(nums) { // https://stackoverflow.com/questions/14636536/how-to-check-if-a-variable-is-an-integer-in-javascript#14794066
   if (!nums) throw new Error("nums is required");
-  let integers = [];
-  nums.forEach(function(num) {
+  let integers = []
+  for (let num of nums) {
     if (!isNaN(num) && parseInt(Number(num)) == num && !isNaN(parseInt(num, 10))) {
       integers.push(num);
     }
-  });
+  }
   return integers;
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  let cities = [];
-  users.forEach(function(user) {
+  let cities = []
+  for (let user of users) {
     cities.push(user.data.city.displayName);
-  });
+  }
   return cities;
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
   let sqrts = [];
-  nums.forEach(function(num) {
+  for (let num of nums){
     sqrts.push(roundTo(Math.sqrt(num), 2));
-  });
+  }
   return sqrts;
 }
 
@@ -71,20 +71,20 @@ function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
   let resultSentence = [];
-  sentences.forEach(function(sentence) {
+  for (let sentence of sentences) {
     if (sentence.toLowerCase().search(str) >= 0) {
       resultSentence.push(sentence);
     }
-  });
+  }
   return resultSentence;
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  let longestSides = [];
-  triangles.forEach(function(triangle) {
+  let longestSides = []
+  for (let triangle of triangles) {
     longestSides.push(Math.max.apply(Math, triangle));
-  });
+  }
   return longestSides;
 }
 
