96ff8c0693cbeb7ed471aaa05cd7608a219cca12
ronnyerrmann <36272471+ronnyerrmann@users.noreply.github.com>
2022-02-15 17:35:54 +0000

    map/filter loops for exercise 4

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -6,11 +6,8 @@ function roundTo(number, digits) {
 
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  let smallNums = [];
-  nums.forEach(function(num) {
-    if (num < 1) {
-      smallNums.push(num);
-    }
+  const smallNums = nums.filter(function(num) {
+    return (num < 1);
   });
   return smallNums;
 }
@@ -18,51 +15,40 @@ function findSmallNums(nums) {
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  let resultNames = [];
-  names.forEach(function(name) {
-    if (name.slice(0,1) == char) {
-      resultNames.push(name);
-    }
+  const resultNames = names.filter(function(name) {
+    return (name.slice(0,1) == char);
   });
   return resultNames;
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  let verbs = [];
-  words.forEach(function(word) {
-    if (word.slice(0,3) == "to ") {
-      verbs.push(word);
-    }
+  const verbs = words.filter(function(word) {
+    return (word.slice(0,3) == "to ");
   });
   return verbs;
 }
 
 function getIntegers(nums) { // https://stackoverflow.com/questions/14636536/how-to-check-if-a-variable-is-an-integer-in-javascript#14794066
   if (!nums) throw new Error("nums is required");
-  let integers = [];
-  nums.forEach(function(num) {
-    if (!isNaN(num) && parseInt(Number(num)) == num && !isNaN(parseInt(num, 10))) {
-      integers.push(num);
-    }
+  const integers = nums.filter(function(num) {
+    return (!isNaN(num) && parseInt(Number(num)) == num && !isNaN(parseInt(num, 10)));
   });
   return integers;
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  let cities = [];
-  users.forEach(function(user) {
-    cities.push(user.data.city.displayName);
+  const cities = users.map(function(user) {
+    return user.data.city.displayName;
   });
   return cities;
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  let sqrts = [];
-  nums.forEach(function(num) {
-    sqrts.push(roundTo(Math.sqrt(num), 2));
+  const sqrts = nums.map(function(num) {
+    return roundTo(Math.sqrt(num), 2);
   });
   return sqrts;
 }
@@ -70,20 +56,16 @@ function getSquareRoots(nums) {
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  let resultSentence = [];
-  sentences.forEach(function(sentence) {
-    if (sentence.toLowerCase().search(str) >= 0) {
-      resultSentence.push(sentence);
-    }
+  const resultSentence = sentences.filter(function(sentence) {
+    return (sentence.toLowerCase().search(str) >= 0);
   });
   return resultSentence;
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  let longestSides = [];
-  triangles.forEach(function(triangle) {
-    longestSides.push(Math.max.apply(Math, triangle));
+  const longestSides = triangles.map(function(triangle) {
+    return Math.max.apply(Math, triangle);
   });
   return longestSides;
 }
